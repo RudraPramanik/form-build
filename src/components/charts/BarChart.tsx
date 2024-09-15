8d0f71007f88e28b48ef1a356e6b363254e5f403
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-15 14:46:06 +0600

    barchart

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -1,60 +1,86 @@
+import { useD3 } from '@/hooks/useD3';
+import React from 'react';
 import * as d3 from 'd3';
-import { useEffect, useRef } from 'react';
 
-const Barchart = () => {
-  const ref = useRef();
+function BarChart({ data }) {
+  const ref = useD3(
+    (svg) => {
+      const height = 500;
+      const width = 500;
+      const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
-  useEffect(() => {
-    // set the dimensions and margins of the graph
-    const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    const svg = d3
-      .select(ref.current)
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-
-    // Parse the Data
-    d3.csv(
-      'https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv'
-    ).then(function (data) {
-      // X axis
       const x = d3
         .scaleBand()
-        .range([0, width])
-        .domain(data.map((d) => d.Country))
-        .padding(0.2);
-      svg
-        .append('g')
-        .attr('transform', `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll('text')
-        .attr('transform', 'translate(-10,0)rotate(-45)')
-        .style('text-anchor', 'end');
+        .domain(data.map((d) => d.year))
+        .rangeRound([margin.left, width - margin.right])
+        .padding(0.1);
 
-      // Add Y axis
-      const y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
-      svg.append('g').call(d3.axisLeft(y));
+      const y1 = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d.sales)])
+        .rangeRound([height - margin.bottom, margin.top]);
 
-      // Bars
+      const xAxis = (g) =>
+        g.attr('transform', `translate(0,${height - margin.bottom})`).call(
+          d3
+            .axisBottom(x)
+            .tickValues(
+              d3
+                .ticks(...d3.extent(x.domain()), width / 40)
+                .filter((v) => x(v) !== undefined)
+            )
+            .tickSizeOuter(0)
+        );
+
+      const y1Axis = (g) =>
+        g
+          .attr('transform', `translate(${margin.left},0)`)
+          .style('color', 'steelblue')
+          .call(d3.axisLeft(y1).ticks(null, 's'))
+          .call((g) => g.select('.domain').remove())
+          .call((g) =>
+            g
+              .append('text')
+              .attr('x', -margin.left)
+              .attr('y', 10)
+              .attr('fill', 'currentColor')
+              .attr('text-anchor', 'start')
+              .text(data.y1)
+          );
+
+      svg.select('.x-axis').call(xAxis);
+      svg.select('.y-axis').call(y1Axis);
+
       svg
-        .selectAll('mybar')
+        .select('.plot-area')
+        .attr('fill', 'steelblue')
+        .selectAll('.bar')
         .data(data)
         .join('rect')
-        .attr('x', (d) => x(d.Country))
-        .attr('y', (d) => y(d.Value))
+        .attr('class', 'bar')
+        .attr('x', (d) => x(d.year))
         .attr('width', x.bandwidth())
-        .attr('height', (d) => height - y(d.Value))
-        .attr('fill', '#5f0f40');
-    });
-  }, []);
+        .attr('y', (d) => y1(d.sales))
+        .attr('height', (d) => y1(0) - y1(d.sales));
+    },
+    [data.length]
+  );
 
-  return <svg width={460} height={400} id="barchart" ref={ref} />;
-};
+  return (
+    <svg
+      ref={ref}
+      style={{
+        height: 500,
+        width: '100%',
+        marginRight: '0px',
+        marginLeft: '0px',
+      }}
+    >
+      <g className="plot-area" />
+      <g className="x-axis" />
+      <g className="y-axis" />
+    </svg>
+  );
+}
 
-export default Barchart;
+export default BarChart;
